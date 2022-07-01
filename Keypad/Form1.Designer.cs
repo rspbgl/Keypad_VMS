44861d2dc6179b88204665d86a3e6b1048d184bf
Ravin Kumar <ravinqwertyuiop@gmail.com>
2022-07-01 14:12:32 +0800

    Add project files.

---
+++ Keypad/Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace Keypad
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.serialPort2 = new System.IO.Ports.SerialPort(this.components);
+            this.SuspendLayout();
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(74, 49);
+            this.textBox1.MaxLength = 3;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(27, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(118, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(557, 309);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = " ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.IO.Ports.SerialPort serialPort2;
+    }
+}
+
---
+++ Keypad/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Keypad
+{
+    public partial class Form1 : Form
+    {
+        string serialDataIn;
+
+        string[] validInputs = { "A01", "A02", "A03", "A04", "A05",
+                                 "A06", "A07" , "A08", "A09", "A10"};        
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            serialPort1.PortName = "COM4";
+            serialPort1.BaudRate = 9600;
+
+            serialPort2.PortName = "COM5";
+            serialPort2.BaudRate = 9600;
+
+            serialPort1.Open();
+            textBox1.Enabled = false;
+        }
+
+        private void serialPort1_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            serialDataIn = serialPort1.ReadExisting(); 
+            this.Invoke(new EventHandler(ShowData));
+        }
+
+        private async void ShowData(object sender, EventArgs e)
+        {
+            textBox1.Text += serialDataIn;
+
+            if (serialDataIn == "B" || textBox1.TextLength > 3)
+            {
+                label1.Text = "Invalid";
+                textBox1.Clear();
+                textBox1.Focus();
+            }
+
+            if (Array.Exists<string>(validInputs, (Predicate<string>)delegate (string s)
+            {
+                return textBox1.Text.IndexOf(s, StringComparison.OrdinalIgnoreCase) > -1;
+            }))
+            {
+                label1.Text = $"{textBox1.Text} Selected!";
+                textBox1.Clear();
+                textBox1.Focus();
+
+                this.Invoke(new EventHandler(button1_Click));
+
+            }
+            else
+            {
+                await Task.Delay(700);
+                label1.Text = "Make Your Selection";
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
